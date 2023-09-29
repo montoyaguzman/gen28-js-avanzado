d4c97442e931be9c2f44e45ccd87351b2970d30d
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-09-28 18:48:39 -0600

    chore: agrega console al primer servidor

--- 1-node/1.4-primer-servidor/index.js
+++ 1-node/1.4-primer-servidor/index.js
@@ -2,10 +2,12 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     const status = '200';
-    const mymeType = { 'Content-type' : 'text/html' };
-    response.writeHead(status, mymeType)
-    response.write('<html> <body> Hola desde node </html> </body>');
+    const mimeType = { 'Content-type' : 'text/html' };
+    response.writeHead(status, mimeType)
+    response.write('<html> <body> Adios desde node </html> </body>');
     response.end();
 });
 
 server.listen(5501);
+
+console.log('Servidor iniciado...');

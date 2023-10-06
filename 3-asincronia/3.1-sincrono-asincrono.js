95aacbb928c5c66b972e841652dce4d4244d0c59
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-05 18:51:30 -0600

    commit: planteamiento de la sincronia

--- 3-asincronia/3.1-sincrono-asincrono.js
+++ 3-asincronia/3.1-sincrono-asincrono.js
@@ -3,20 +3,24 @@
  * Una linea se ejecuta inmediatamente despues de la otra
  * no hay codigo dependiente o bloqueante.
  */
-console.log('============== BLOQUE 1 ==============');
-console.log('Primero'); 
-console.log('Segundo');
-console.log('Tercero');
+// console.log('============== BLOQUE 1 ==============');
+console.log('1 Damos click en pagar');
+for(let i=0; i<100; i++) {
+    console.log('2 Validando pago...');
+    // Esto no es sostenible en la vida real ya que se necesita usar
+    // cosas externar como Apis, WEB Api, acceso al sistema operativo, etc
+} 
+console.log('3 Todo joya!!');
 
 
 /**
  * BLOQUE 2 ASINCRONO
  */
 console.log('============== BLOQUE 2 ==============');
-console.log('Primero'); 
+console.log('1 Damos click en pagar'); 
 
-setTimeout(() => {
-    console.log('Segundo');
+setTimeout(() => { // setTimeout va a ser la representacion de ese codigo dependiente (asincrono)
+    console.log('2 Validando pago...');
 }, 0);
 
-console.log('Tercero');
+console.log('3 Todo joya!!');

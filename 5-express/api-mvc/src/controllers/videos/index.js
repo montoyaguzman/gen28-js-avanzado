93bcb3fcddaea2e3ffe37c70aad9af46f1c6f44b
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-11 21:20:01 -0600

    feat: agrega la carpeta controllers

---
+++ 5-express/api-mvc/src/controllers/videos/index.js
@@ -0,0 +1,57 @@
+const videos = [
+    { id: 1, name: 'Kilometros', artist: 'Los Caligaris' },
+    { id: 2, name: 'Closer', artist: 'The Chainsmokers' },
+    { id: 3, name: 'Solo a Terceros', artist: 'Panda' },
+];
+
+const getAllVideos = (req, res) => {
+    res.json(videos);
+};
+
+const getOneVideo = (req, res) => {
+    console.log(req.query.pageSize);
+    console.log(req.query.paginationKey);
+    const id = req.params.id;
+    const foundedVideo = videos.find(element => element.id === parseInt(id));
+    res.json(foundedVideo);
+};
+
+const createVideo = (req, res) => {
+    console.log('req: ', req);
+    console.log('req.body: ', req.body);
+    const newBody = req.body;
+    videos.push(newBody);
+    res.status(201).json({ message: 'todo gucci!', song: newBody.name });
+}
+
+const modifyAllVideo = (req, res) => {
+    const id = req.params.id;
+    const newBody = req.body;
+    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
+    videos[foundedPosition] = newBody;
+    res.status(200).json({ id });
+};
+
+const modifyPartialVideo = (req, res) => {
+    const id = req.params.id;
+    const newBody = req.body;
+    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
+    videos[foundedPosition] = { ...videos[foundedPosition], ...newBody };
+    res.status(200).json({ id });
+};
+
+const deleteVideo = (req, res) => {
+    const id = req.params.id;
+    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
+    videos.splice(foundedPosition, 1);
+    res.status(200).json({ id });
+};
+
+module.exports = {
+    getAllVideos,
+    getOneVideo,
+    createVideo,
+    modifyAllVideo,
+    modifyPartialVideo,
+    deleteVideo,
+}
--- 5-express/api-mvc/src/routes/videos/index.js
+++ 5-express/api-mvc/src/routes/videos/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const videosRouter = express.Router();
+const {
+  getAllVideos,
+  getOneVideo,
+  createVideo,
+  modifyAllVideo,
+  modifyPartialVideo,
+  deleteVideo,
+} = require('../../controllers/videos/');
 
 const videos = [
     { id: 1, name: 'Kilometros', artist: 'Los Caligaris' },
@@ -8,52 +16,21 @@ const videos = [
 ];
 
 // GET: Obtener todos los videos
-videosRouter.get('/', (req, res) => {
-    res.json(videos);
-  });
+videosRouter.get('/', getAllVideos);
   
-  // GET ONE
-  videosRouter.get('/:id', (req, res) => {
-    console.log(req.query.pageSize);
-    console.log(req.query.paginationKey);
-    const id = req.params.id;
-    const foundedVideo = videos.find(element => element.id === parseInt(id));
-    res.json(foundedVideo);
-  });
-  
-  // POST: Crear un video recibido desde el request
-  videosRouter.post('/', (req, res) => {
-    console.log('req: ', req);
-    console.log('req.body: ', req.body);
-    const newBody = req.body;
-    videos.push(newBody);
-    res.status(201).json({ message: 'todo gucci!', song: newBody.name });
-  });
-  
-  // PUT: Modificacion del recurso completo
-  videosRouter.put('/:id', (req, res) => {
-    const id = req.params.id;
-    const newBody = req.body;
-    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
-    videos[foundedPosition] = newBody;
-    res.status(200).json({ id });
-  });
-  
-  // PATCH: Modificacion parcial del recurso
-  videosRouter.patch('/:id', (req, res) => {
-    const id = req.params.id;
-    const newBody = req.body;
-    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
-    videos[foundedPosition] = { ...videos[foundedPosition], ...newBody };
-    res.status(200).json({ id });
-  });
-  
-  // DELETE: Eliminar un recurso
-  videosRouter.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    const foundedPosition = videos.findIndex(element => element.id === parseInt(id));
-    videos.splice(foundedPosition, 1);
-    res.status(200).json({ id });
-  });
+// GET ONE
+videosRouter.get('/:id', getOneVideo);
 
-  module.exports = { videosRouter };
+// POST: Crear un video recibido desde el request
+videosRouter.post('/', createVideo);
+
+// PUT: Modificacion del recurso completo
+videosRouter.put('/:id', modifyAllVideo);
+
+// PATCH: Modificacion parcial del recurso
+videosRouter.patch('/:id', modifyPartialVideo);
+
+// DELETE: Eliminar un recurso
+videosRouter.delete('/:id', deleteVideo);
+
+module.exports = { videosRouter };

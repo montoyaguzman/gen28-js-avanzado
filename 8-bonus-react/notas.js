f36e1e3b29c43f798150529c31bf71634de988c9
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-20 20:37:52 -0600

    feat: notas prework para react

---
+++ 8-bonus-react/notas.js
@@ -0,0 +1,75 @@
+// 1. Fundamentos de programacion
+// 2. Fundamentos JS
+//     funciones, metodos y parametros, return
+
+// 3. POO 
+//     definicion de clases (sugar syntaxis)
+//     creacion de objetos
+//     llamar a metodos del objeto
+
+// 4. ES6
+    // literal templates 
+    console.log(`hola ${name}`);
+    // import/export ( {}, default, * as )
+    // Short hand names
+        const name = 'montoya';
+        const sensei = { name };
+    // arrow functions
+        function hello() {
+
+        }
+        const hello = () => {
+            // bla bla 
+            // bla bla
+            return a;
+        };
+        const hello = () => a
+    // map y forEach
+    // filter, find, findIndex, splice, push
+    // spread operator ...
+    // destructuring
+        const create = (schemaSong) => {
+            console.log(schemaSong)
+        };
+        
+        const body = {
+        author: 'karol g',
+        song: 'bichota',
+        album: 'bichota 2.0',
+        anio: 2020
+        };
+        
+        /*
+        const author =  body.author;
+        const song =  body.song;
+        const album =  body.album;
+        const anio =  body.anio;
+        */
+        const { author, song, album, anio: myName } = body;
+        
+        const schemaSong = {
+        author1: author,
+        song1: song,
+        album1: album,
+        anio1: myName
+        }
+        
+        create(schemaSong);
+    // optional chaining
+    // if (obj.adress.street.name)
+    // if (obj && obj.adress && obj.adress.street && obj.adress.street.name)
+    // if (obj?.adress?.street?.name)
+
+// 5. Render dinamico del html
+        // createElement, appendChild
+
+// 6. eventos
+        // addeventlister
+
+// 7. node y npm
+        // scaffold
+        // package.json
+        // package-lock.json
+        // node_modules
+
+// reactJS
